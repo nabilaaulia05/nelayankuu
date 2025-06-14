3c5996e057418cd9983e5dc67f8b92ec71e019a8
nabila aulia <nabilaauliaramadhani.teknokrat@gmail.com>
2025-06-14 22:45:13 +0700

    commit 2 nabila

---
+++ app/src/main/java/com/bunga/nelayanku/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.bunga.nelayanku
+
+package com.example.nelayanku
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
